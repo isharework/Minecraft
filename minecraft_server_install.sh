68feac93ad26f41116345c89ba6719cc724b676b
isharework <isharework@users.noreply.github.com>
2018-06-21 15:39:10 +0800

    将友好提示移至后面

--- minecraft_server_install.sh
+++ minecraft_server_install.sh
@@ -13,7 +13,9 @@ mkdir minecraft
 cd minecraft
 wget --no-check-certificate -O minecraft_server.1.12.2.jar http://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar
 #4.启动我的世界服务端
-echo -e "将启动我的世界服务端,若是出现You need to agree to the EULA in order to run the server. Go to eula.txt for more info.
+echo -e "将启动我的世界服务端";
+wget --no-check-certificate -O minecraft_server_run.sh https://raw.githubusercontent.com/isharework/Minecraft/master/minecraft_server_run.sh && bash minecraft_server_run.sh
+echo -e "若是出现You need to agree to the EULA in order to run the server. Go to eula.txt for more info.
 请修改当前目录下的eula.txt 、server.properties文件.
 eula.txt文件的修改:
 eula=false   
@@ -22,4 +24,3 @@ server.properties文件的修改:
 加上online-mode=false
 修改完成后再次运行脚本即可 nohup sh minecraft_server_run.sh &
 ";
-wget --no-check-certificate -O minecraft_server_run.sh https://raw.githubusercontent.com/isharework/Minecraft/master/minecraft_server_run.sh && bash minecraft_server_run.sh
