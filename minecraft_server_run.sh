b7d0ce0c85eacded10f3ff5d85dd33d6d4d1c4a4
isharework <isharework@users.noreply.github.com>
2018-06-21 16:01:42 +0800

    4

--- minecraft_server_run.sh
+++ minecraft_server_run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xmx1536M -Xms1024M -jar -Dfile.encoding=utf-8 -Duser.timezone=Asia/Shanghai /root/minecraft/minecraft_server.1.12.2.jar nogui
+java -Xmx1536M -Xms1024M -jar -Dfile.encoding=utf-8 -Duser.timezone=Asia/Shanghai minecraft_server.1.12.2.jar nogui
